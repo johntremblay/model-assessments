667df4b477e3a0144014da52e844536315ef8f73
John <jonathan.tremblay.11@gmail.com>
2020-05-17 09:41:03 -0400

    final testing

---
+++ models/linear_model_torch.py
@@ -0,0 +1,12 @@
+from torch import nn
+import torch
+import math
+
+class Mnist_Logistic(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(784, 10) / math.sqrt(784))
+        self.bias = nn.Parameter(torch.zeros(10))
+
+    def forward(self, xb):
+        return xb @ self.weights + self.bias
